0b6fef27e8117eb27c2ecf310e6450f51f167a5c
derikincode <derikoliveira252@gmail.com>
2024-05-13 15:49:05 -0300

    Correção

--- src/db/connection.php
+++ src/db/connection.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $dbname = ""; 
 
-$connection = new myqli($servername, $username, $password, $dbname);
+$connection = new mysqli($servername, $username, $password, $dbname);
 
 if ($connection -> connect_error) {
     die("Connection failed: " . $connection -> connect_error);
